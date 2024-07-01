b2699d65f796ce52e4c54c215db1865f1ff04f6d
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 09:53:36 +0200

    Update stim.h

--- NE_Ref/Exercises/Exercise2/stim.h
+++ NE_Ref/Exercises/Exercise2/stim.h
@@ -2,20 +2,27 @@
 #define STIMULUS_H
 
 #include <systemc.h>
-
+/****************************************************************************************************************************************
+ *                     Implementing Stim Module for stimulating values to the Inputs of the exor module to test it                      *
+ ****************************************************************************************************************************************/
 SC_MODULE(stim)
 {
 public:
+    //those will be considered as Input values for the SC_IN A and SC_IN B of the exor module to test it
     sc_out<bool> A, B;
 
     SC_CTOR(stim)
     {
+        // defining a thread that will generate values on the I/P ports A & B of the exor module to test it
         SC_THREAD(StimGen);
     }
 
 private:
+    // Implementing the stimuli generation function
+    // we used threads here as we cant use the wait statement with the System C methods
     void StimGen()
     {
+        //here we write values to port A and B for testing
         wait(SC_ZERO_TIME);
         A.write(false);
         B.write(false);
