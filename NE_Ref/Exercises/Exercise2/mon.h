93aa217982574f6ead76a993a9d88386b257593c
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 09:57:35 +0200

    Update mon.h

--- NE_Ref/Exercises/Exercise2/mon.h
+++ NE_Ref/Exercises/Exercise2/mon.h
@@ -3,16 +3,21 @@
 
 #include <iostream>
 #include <systemc.h>
-
+/****************************************************************************************************************************************
+ *                  Implementing Monitor Module that prints any change that happens for the I/P and O/P ports of the exor module        *
+ ****************************************************************************************************************************************/
 //Senstive to any change in A,B,Z and it prints it.
 SC_MODULE(Monitor)
 {
 public:
+    // defining 3 input signals that enter to the enter module to show the change that happens within the I/P and O/P of the Exor module
     sc_in<bool> A, B, Z;
 
     SC_CTOR(Monitor)
     {
+        // here we print the initial values of the I/P and O/P of the exor module
         std::cout << std::endl <<  "time\tA\tB\tF" << std::endl;
+        //defining sc method called monitor that is sensitive to any change and we dont run it in the beggining of simulation.
         SC_METHOD(monitor);
         sensitive << A << B << Z;
         dont_initialize();
@@ -21,6 +26,7 @@ public:
 private:
     void monitor()
     {
+        //everytime a change happen with the I/P and O/P Signals it will be printed
         std::cout << sc_time_stamp()  << "\t" << A << "\t" << B << "\t" << Z << std::endl;
     }
 };
