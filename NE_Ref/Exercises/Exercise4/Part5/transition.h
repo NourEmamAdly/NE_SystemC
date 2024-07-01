43ff86b5de8541ae535a54ed93afdf4382162ce9
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 13:56:27 +0200

    Update transition.h

--- NE_Ref/Exercises/Exercise4/Part5/transition.h
+++ NE_Ref/Exercises/Exercise4/Part5/transition.h
@@ -9,7 +9,7 @@ using namespace std;
 // Transition:
 
 //In order to have more than one port for in and out we template the transistion module as follows to support multiple input ports
-// N is the number of input ports , M is the number of output ports and an inhebitor
+// N is the number of input ports , M is the number of output ports and an inhebitor to check if there is a certain value at it or not to perform the functions
 template<unsigned int N=1, unsigned int M=1, unsigned int L=0>
 SC_MODULE(transition)
 {
@@ -22,63 +22,49 @@ sc_port<placeInterface, L, SC_ZERO_OR_MORE_BOUND> inhibitors;
 
 //defining constructor of the transistion module we are creating here
 public:
-SC_CTOR(transition){};
+ SC_CTOR(transition): in("in"), out("out"), inhibitors("inhibitors"){}
 
 //implementing a function here called fire that checks how many inputs ports that have at least one token
 // and do other checks and print output
 public:
 void fire()
 {
-    unsigned int conditions_satisfied = 1;
+ //check Places
+        int counterToken = 0;
+        int counterInh = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (in[i]->testTokens())
+            {
+                counterToken++;
+            }
+        }
 
-for (int i=0;i<N;i++)
-{
-    bool toks= in[i]->testTokens();
+        
+        for (int i = 0; i < L; i++)
+        {
+            if (inhibitors[i]->getTokens() > 0)
+            {
+                counterInh++;
+            }
+        }
 
-    if (toks == 0)
-    {
+        //if all previous places have tokens
+        if ((counterToken == N) && (counterInh == 0))  //----- Task 5
+        {
+            cout << this->name() << ": Fired" << endl;
 
-        conditions_satisfied = 0;
+            for (int i = 0; i < N; i++){
+                in[i]->removeTokens();
+            }
+            for (int i = 0; i < M; i++){
+                out[i]->addTokens();
+            }
 
+        }else{
+            cout << this->name() << ": NOT Fired" << endl;
+        }
     }
-}
-for (int i=0; i<L;i++)
-{
-bool inhibs = inhibitors[i]->testTokens();
-if (inhibs == 1)
-{
-    conditions_satisfied =0;
-}
-
-}
-if (conditions_satisfied == 1)
-{
-cout << this->name() <<": Fired" <<endl;
-
-for(int i=0;i<N;i++)
-{
-    in[i]->removeTokens();
-
-}
-for(int i=0;i<M;i++)
-{
-    out[i]->addTokens();
-    
-}
-
-}
-else
-{
-
-cout << this->name() <<":NOT Fired" <<endl;
-
-}
-
-
-
-}
-
-
 
 };
 
