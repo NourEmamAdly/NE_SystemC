8f22599693b0fe2de4b6dc3bba030cb936be0614
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 12:30:09 +0200

    Update main.cpp

--- NE_Ref/Exercises/Exercise4/Part1/main.cpp
+++ NE_Ref/Exercises/Exercise4/Part1/main.cpp
@@ -3,11 +3,14 @@
 #include "place.h"
 #include "transition.h"
 #include "subnet.h"
-
+/****************************************************************************************************************************************
+ * Implementing Top Level Module that contains more than one transition module connected to each other using the place channel we drived*
+ *                                            Check the figure 1 Simple Petri Net                                                       *
+ ****************************************************************************************************************************************/
 // Toplevel:
 SC_MODULE(toplevel)
 {
-    
+//Inistentiting two transition objects called t1 and t2  and place channels p1 and p2 to connect between the two objects
 private:
 transition t1;
 transition t2;
@@ -16,6 +19,7 @@ place p1,p2;
 public:
 SC_CTOR(toplevel): t1("t1"),t2("t2"),p1(1),p2(0)
 {
+// bind our transition modules to the place channels as shown in the fig 1 (Simple Petri Net)
     t1.in.bind(p1);
     t1.out.bind(p2);
     t2.in.bind(p2);
@@ -24,7 +28,7 @@ SC_CTOR(toplevel): t1("t1"),t2("t2"),p1(1),p2(0)
 }
 
 
-
+// Implementing a process to test our transistion modules that is connected to the custom channels that we designed 
 void process()
 {
 
@@ -45,6 +49,7 @@ void process()
 };
 
 
+// Instintiating a top level module to test our compination
 
 int sc_main(int, char**)
 {
