fa6e4157d461d8e29f47fcd51195036995105832
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 12:54:21 +0200

    Update transition.h

--- NE_Ref/Exercises/Exercise4/Part2/transition.h
+++ NE_Ref/Exercises/Exercise4/Part2/transition.h
@@ -17,19 +17,19 @@ SC_MODULE(transition)
 public:
 sc_port<placeInterface, N,SC_ALL_BOUND> in;
 sc_port<placeInterface, M,SC_ALL_BOUND> out;
-
+sc_port<placeInterface> in[N];
 public:
 SC_CTOR(transition): in("in") ,out("out"){}
 
 public:
 void fire()
 {
-int counterToken=0;
-unsigned int temp =in->testTokens();
 
+int counterToken=0;
+//check if every input port have at least one token and count how many input ports that have at least one token
 for (int i=0;i<N;i++)
 {
-    if (temp >=1)
+    if (in[i]->testTokens() >= 1)
     {
 
         counterToken++; //Count how many input ports that have at least one token
@@ -40,31 +40,31 @@ for (int i=0;i<N;i++)
 }
 
 
-if (counterToken >= N)
-{
+if (counterToken == N) //^If all previous ports have a token, then...
+        {
+          // if all previous places have tokens
+            cout << this->name() << ": Fired" << endl;
+         // Then remove from each token from each input port and 
+            for (int i = 0; i < N; i++){
+                in[i]->removeTokens(1);     //----Task 1 and 2
+                                            //^Remove tokens from previous place
+            }
+         // Then add token to each output port
+            for (int i = 0; i < M; i++){
+                out[i]->addTokens(1);  //-----Task 1 and 2
+                                        //^Add token to next place
+            }
 
-cout << this->name() << ": Fired" <<endl;
+        }
+// otherwise print that this port is not fired
+else{
+            cout << this->name() << ": NOT Fired" << endl;
+        }
+    }
+
+};
 
 
-for (int j=0;j<N;j++)
-{
-in[j]->removeTokens(1);
-}
-
-for (int k=0;k<N;k++)
-{
-out[k]->addTokens(1);
-}
-
-}
-
-else
-{
-cout << this->name() <<":NOT Fired" <<endl;
-
-}
-
-}
 
 
 
