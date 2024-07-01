f915afabec8cb93c1c4c53943b71183dcb468bf0
Nour Emam Adly <59049925+NourEmamAdly@users.noreply.github.com>
2024-07-01 12:06:11 +0200

    Update place.h

--- NE_Ref/Exercises/Exercise4/Part1/place.h
+++ NE_Ref/Exercises/Exercise4/Part1/place.h
@@ -4,42 +4,51 @@
 #include <systemc.h>
 
 // Place Interface:
-
-class placeInterface : public sc_interface
+/****************************************************************************************************************************************
+ *                        Implementing custom Place Interface and Place Channel and ports                                               *
+ ****************************************************************************************************************************************/
+// Deriving the place interface from the SC Interface standrd
+class placeInterface : public sc_interface  // Creating Place Custom Interface
 {
+
+//defining the pure virtual functions that will be implemented in the drived channel
 public:
 virtual void addTokens(unsigned int n)=0;
 virtual void removeTokens(unsigned int n)=0;
 virtual unsigned int testTokens()=0;
 
-
-
 };
 
 
-// Place Channel:
+// Deriving a channel called place from the place Interface that we created up
 class place :public placeInterface
 {
 
 private:
-
 unsigned int tokens;
 
 public:
+//defining the place channel constructor
 place(unsigned int initial_tokens = 1): tokens(initial_tokens)
 {
     //one way of implementation
 }
+
+                                    // Implementing the pure virtual functions mentioned in the Interface
+ 
+//Here the function is adding n tokens to the current value of the tokens
 void addTokens(unsigned int n)
 {
     tokens+=n;
 }
 
+//Here the function is subtracting n tokens to the current value of the tokens
 void removeTokens (unsigned int n)
 {
     tokens-=n;
 }
 
+//Here the function is returning the current value of the tokens we have
 unsigned int testTokens()
 {
 
